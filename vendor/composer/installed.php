4e5d1ba5765d6d59cc52a97a75b759ab25f1c69c
AhmedQotb <50117886+AhmedQotbAhmed@users.noreply.github.com>
2023-09-23 16:21:42 +0300

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'almasaeed2010/adminlte',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '32869efafafde2d718db6556eb3cf7193fde8267',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'almasaeed2010/adminlte' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '32869efafafde2d718db6556eb3cf7193fde8267',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
